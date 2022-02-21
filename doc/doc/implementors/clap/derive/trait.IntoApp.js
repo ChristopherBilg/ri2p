26ab8d5795fde13f07c64a07f66fb29a146b8c7b
ChristopherBilg <ChristopherBilg@users.noreply.github.com>
2022-02-21 12:43:55 +0000

    deploy: 18f1135ec1ee2d2465600f16355e5ccc447311d9

--- doc/doc/implementors/clap/derive/trait.IntoApp.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["ri2p"] = [{"text":"impl <a class=\"trait\" href=\"clap/derive/trait.IntoApp.html\" title=\"trait clap::derive::IntoApp\">IntoApp</a> for <a class=\"struct\" href=\"ri2p/daemon/program_arguments/struct.ProgramArguments.html\" title=\"struct ri2p::daemon::program_arguments::ProgramArguments\">ProgramArguments</a>","synthetic":false,"types":["ri2p::daemon::program_arguments::ProgramArguments"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
